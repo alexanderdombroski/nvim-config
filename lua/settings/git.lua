7db0514c25b12ecdaa9d709367962e85efe214e0
alexanderdombroski <dom21013@byui.edu>
2024-11-07 20:45:04 -0700

    Added shortcuts to run git stash commands

--- lua/settings/git.lua
+++ lua/settings/git.lua
@@ -11,9 +11,18 @@ end, { desc = "Save and stage current file" })
 vim.keymap.set("n", "<leader>ga.", function()
   vim.cmd("wa")               -- Save all files
   vim.cmd("Git add .")        -- Stage all changes
+  vim.cmd("echo 'Saved and staged All Files'")
 end, { desc = "Save all and stage all files" })
 
-vim.keymap.set("n", "<leader>gc", ":Git commit")
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
+
+-- Git Stash
+vim.keymap.set("n", "<leader>gss", function()
+    vim.cmd("wa")
+    vim.cmd("Git stash -u")
+end, {desc = "Save and Stash all files"})
+vim.keymap.set("n", "<leader>gsp", ":Git stash pop<CR>")
+
 
 
